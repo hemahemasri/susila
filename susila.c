02483836b13ccd05f74a3c8df24a297b7a078638
hemahemasri <30549523+hemahemasri@users.noreply.github.com>
2017-08-08 16:03:28 +0530

    Create susila.c

---
+++ susila.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n;
+printf("enter the values of n");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("the given number is even");
+else 
+{
+printf("the given numbers are odd");
+}
+return 0;
+getch();
+}
